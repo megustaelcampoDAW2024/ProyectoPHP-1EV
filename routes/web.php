1babdcf97ba87bfc5a2f386456fcd6a1751449eb
PcClase <pablobejarano2005@gmail.com>
2024-11-25 11:49:12 +0100

    Prueba Commit PcClase

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Prueba PcClase
+Route::get('index', function () {
+    return view('welcome');
+});
+
 Route::any('hola', function () {
     return view('hola');
 });
